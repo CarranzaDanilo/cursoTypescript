1e678da616a72f20cf89685b6cb5da851bb665cc
unknown <leos_87@hotmail.com>
2023-11-05 13:43:24 -0300

    increased by map

--- src/app/pages/arrays/arrays.component.ts
+++ src/app/pages/arrays/arrays.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,12 +10,14 @@ import { ProductService } from 'src/app/services/product.service';
 export class ArraysComponent implements OnInit {
   productService = inject(ProductService);
   products: Product[] = [];
+  productsIncreased: Product[] = [];
   myArray: number[] = [];
 
   ngOnInit(): void {
     this.getPrices();
     this.getProducts();
   }
+
 
   getPrices() {
     let prices = [12, 25, 36, 89, 96, 21, 15];
@@ -25,13 +27,21 @@ export class ArraysComponent implements OnInit {
     this.myArray = prices;
   }
 
-  getProducts(){
-    this.productService.getAllProducts().then(resp => {
-      this.products = resp;
-    }).catch( error => {
-      console.log(error);
-    })
+  getProducts() {
+    this.productService
+      .getAllProducts()
+      .then((resp: Product[]) => {
+        this.products = resp;
+        // Mapea los productos y guárdalos en productsIncreased cuando los datos estén disponibles.
+        this.productsIncreased = this.products.map((item) => {
+          return {
+            ...item,
+            price: item.price * 1.2,
+          };
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-
-
 }
